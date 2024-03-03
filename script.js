1027e6a9c9110426dfb3d47bea61695da5c93136
ghuraba <albsvids@gmail.com>
2024-03-03 21:32:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// script.js
+// Initialize Firebase
 var config = {
     apiKey: "AIzaSyDpsobzDm9421BbdvPDOInoZJf672FeZfw",
     authDomain: "flepe-6dc02.firebaseapp.com",
@@ -12,21 +12,8 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-function animateProgressBar(progressBar, duration, targetWidth) {
-    let progress = 0;
-    const increment = targetWidth / duration * 100; // Adjust the increment value for desired smoothness
-    const interval = setInterval(() => {
-        progress += increment;
-        progressBar.style.width = `${progress}%`;
-        if (progress >= targetWidth) {
-            clearInterval(interval);
-        }
-    }, 10); // Adjust the interval duration for desired speed
-}
-
-// script.js
+// Function to update the progress bar
 function updateProgressBar() {
-    // Listen for changes in the votes
     database.ref('votes').on('value', function(snapshot) {
         var votes = snapshot.val();
         var flokiVotes = votes.floki || 0;
@@ -37,15 +24,8 @@ function updateProgressBar() {
             var flokiPercentage = (flokiVotes / totalVotes) * 100;
             var pepePercentage = (pepeVotes / totalVotes) * 100;
             
-            // Update Floki bar
-            var flokiBar = document.getElementById('floki-bar');
-            flokiBar.style.width = flokiPercentage + '%';
-            flokiBar.setAttribute('data-full', flokiPercentage >= 100);
-            
-            // Update Pepe bar
-            var pepeBar = document.getElementById('pepe-bar');
-            pepeBar.style.width = pepePercentage + '%';
-            pepeBar.setAttribute('data-full', pepePercentage >= 100);
+            document.getElementById('floki-bar').style.width = flokiPercentage + '%';
+            document.getElementById('pepe-bar').style.width = pepePercentage + '%';
         }
     });
 }
